bb8f02a9ef3f2ac259a11595480f90de8ea11f14
ifjika <jikapramuja221@gmail.com>
2025-06-21 22:11:44 +0700

    Add route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// App.jsx
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Services from "./pages/Services";
 import PriceList from "./pages/PriceList";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/price-list" element={<PriceList />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/price-list" element={<PriceList />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about-us" element={<AboutUs />} />
+    </Routes>
   );
 }
 
